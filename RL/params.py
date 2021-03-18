e1334b698742bf4c2d49ed9f89dc97a64d30db81
sergfer26 <sfernandezm97@ciencias.unam.mx>
2021-03-18 15:39:58 -0600

    actualice

--- RL/params.py
+++ RL/params.py
@@ -1,11 +1,11 @@
 PARAMS_ENV = {'R': 'min(0.01 * h, 10)' , \
               'P': '- (0.5/5) * (u3 + u4 + u7 + u9 + u10)' , \
               'STEP': 1/8, \
-              'TIME_MAX': 90, \
+              'TIME_MAX': 15, \
               'FRECUENCY': 60, 
               'MONTH': '03'}
 
-PARAMS_TRAIN = {'EPISODES': 500, \
+PARAMS_TRAIN = {'EPISODES': 5, \
                 'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
