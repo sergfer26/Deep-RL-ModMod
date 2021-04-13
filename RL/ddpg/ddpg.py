3febbc19d59dfb4d1eb2d8c1120ce9b2be3e9eb1
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-04-13 13:48:21 -0500

    Se hicieron pruebas

--- RL/ddpg/ddpg.py
+++ RL/ddpg/ddpg.py
@@ -61,8 +61,8 @@ class DDPGagent:
     def get_action(self, state):
         state = Variable(torch.from_numpy(state).float().unsqueeze(0))
         action = self.actor.forward(state.to(device))
-        # action = action.detach().cpu().numpy()[0,:]
-        action = action.detach().numpy()[0,:]
+        action = action.detach().cpu().numpy()[0,:]
+        #action = action.detach().numpy()[0,:]
         return action
 
     def update(self, batch_size):
