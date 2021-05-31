21d33ab142507d21387a04ecff36aff4c4534a1b
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-05-31 13:36:18 -0500

    Se cambio grafica de barras por histograma

--- RL/get_report_agents.py
+++ RL/get_report_agents.py
@@ -105,7 +105,7 @@ def create_report(PATH):
     add_image(PATH,pdf,'/histograms_mass.png',10,0,600,600)
     pdf.showPage()
     #Siguiente pagina
-    add_image(PATH,pdf,'/scores_reward.png',10, 350,600,600)
+    add_image(PATH,pdf,'/histograms_reward.png',10, 350,600,600)
     add_text(pdf,[PATH],10,60)
     pdf.showPage()
     #Siguiente pagina
