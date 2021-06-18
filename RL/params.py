2a601b7db32e14205e47d0e6b8ccd47ffcc5ae5c
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-06-17 21:02:16 -0500

    Se agrego comentario

--- RL/params.py
+++ RL/params.py
@@ -7,7 +7,7 @@ PARAMS_ENV = {'R': 'min(0.01 * h, 10)' , \
 
 #Para entrenamiento MONTH puede ser '03','06','09' o 'RANDOM'
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
-
+#El min de STEP es 1/24
 PARAMS_TRAIN = {'EPISODES': 500, \
                 'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
                 'BATCH_SIZE': 128, \
