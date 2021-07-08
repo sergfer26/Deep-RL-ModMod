2951a259f76493669a45ba887a453817730c603c
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-07-07 21:42:33 -0500

    Parametros del Modelo climatico

---
+++ RL/params_climate.py
@@ -0,0 +1,10 @@
+PARAMS_CLIMATE = {
+    'q_gas': 2.45,        # Precio de gas natural
+    'n_gas': 35.26,       # Efiencia energetica del gas natural
+    'lamb4': 5*(10**5),   # Capacidad calorifica del calentador de aire directo
+    'alpha6':10**4,       # Area de la supercie del piso del invernadero
+    'phi1':  51e-3,       # Diametro externo de la tubera de calentamiento
+    'gamma1':1.875,       # Longitud de la tubera de calentamiento. Tenia el valor de Almeria
+    'alpha3':0.3,         # Superficie de la tuberia de calentamiento
+    'psi2':  7.2*(10**4)  # Capacidad de la fuente externa de CO2. Tenia el valor de Texas
+}
