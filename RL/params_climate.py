2e5321b96a5eae91c4d7caca702110082703754b
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-07-12 23:05:10 -0500

    Se agrego T_cal

--- RL/params_climate.py
+++ RL/params_climate.py
@@ -1,10 +1,12 @@
 PARAMS_CLIMATE = {
-    'q_gas': 2.45,        # Precio de gas natural
-    'n_gas': 35.26,       # Efiencia energetica del gas natural
+    'qgas': 2.45,        # Precio de gas natural
+    'etagas': 35.26,       # Efiencia energetica del gas natural
     'lamb4': 5*(10**5),   # Capacidad calorifica del calentador de aire directo
     'alpha6':10**4,       # Area de la supercie del piso del invernadero
     'phi1':  51e-3,       # Diametro externo de la tubera de calentamiento
     'gamma1':1.875,       # Longitud de la tubera de calentamiento. Tenia el valor de Almeria
     'alpha3':0.3,         # Superficie de la tuberia de calentamiento
-    'psi2':  7.2*(10**4)  # Capacidad de la fuente externa de CO2. Tenia el valor de Texas
+    'psi2':  7.2*(10**4),  # Capacidad de la fuente externa de CO2. Tenia el valor de Texas
+    'q_co2_ext' :3.5,
+    'T_cal': 95
 }
