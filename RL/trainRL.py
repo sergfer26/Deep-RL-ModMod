b47ebc7e73a64de05eb5f768034bf1b2bcdf55a8
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2021-07-19 15:28:22 -0500

    quite bibliotecas sin usar y agregue formato

--- RL/trainRL.py
+++ RL/trainRL.py
@@ -1,44 +1,40 @@
-from __future__ import barry_as_FLUFL
 import sys
-from typing_extensions import final
-import gym
-import pytz
 import numpy as np
-import pandas as pd
-import pathlib
 import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib import dates
 from env import GreenhouseEnv, STEP, TIME_MAX
 from ddpg.ddpg import DDPGagent
 from ddpg.utils import *
 from tqdm import tqdm
-from math import ceil
-from datetime import datetime, timezone
 from time import time
 from correo import send_correo
-import json
 from time import time
-#from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
+# from typing_extensions import final
 from get_report import create_report
 from params import PARAMS_TRAIN
-#from reward import G
-from graphics import save_Q,figure_reward,figure_state,figure_rh_par,figure_prod,figure_actions,figure_inputs,compute_indexes,create_path, save_rewards,figure_cost_gain
+from __future__ import barry_as_FLUFL
+from graphics import save_Q, figure_reward, figure_state
+from graphics import figure_rh_par, figure_prod, figure_actions 
+from graphics import figure_inputs, compute_indexes, create_path
+from graphics import save_rewards,figure_cost_gain
+
+
 EPISODES = PARAMS_TRAIN['EPISODES']
 STEPS = PARAMS_TRAIN['STEPS']
 BATCH_SIZE = PARAMS_TRAIN['BATCH_SIZE']
 SHOW = PARAMS_TRAIN['SHOW']
 INDICE = PARAMS_TRAIN['INDICE'] #Cero para entrenar y 8770 para probar
 
+
 env = GreenhouseEnv()
 agent = DDPGagent(env)
-
 noise = OUNoise(env.action_space)
 action_dim =  env.action_space.shape[0]
 state_dim = env.observation_space.shape[0]
 #writer_reward = SummaryWriter()
 #writer_abs = SummaryWriter()
 #writer_penalty = SummaryWriter()
+
 
 if not SHOW:
     from functools import partialmethod
@@ -121,7 +117,6 @@ def sim(agent, env, indice = 0):
     return S_climate, S_data, S_prod, A, data_inputs,start
 
 
-
 def main():
     t1 = time()
     mpl.style.use('seaborn')
@@ -153,6 +148,7 @@ def main():
     if not(SHOW):
         create_report(PATH,t2-t1)
         send_correo(PATH + '/Reporte.pdf')
+
 
 if __name__=='__main__':
     main()
