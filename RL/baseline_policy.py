83ad0e16737e627694b575ee61d8f8e7587d283d
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2021-08-06 17:56:55 -0500

    falta agregar controles a agente

--- RL/baseline_policy.py
+++ RL/baseline_policy.py
@@ -1,15 +1,21 @@
 import numpy as np
+from climate_model.functions import q2
 
 KP4 = 3.672
 KI4 = 188.659
 KD4 = 0.00
+
 KP10 = 0.019
 KI10 = 6033.599
 KD10 = 65.103
 
+KP8 = -4.414
+KI8 = 3043.253
+KD8 = 47.512
+
 VENTSET1 = 25
 VENTSET2 = 25
-PAR = 5
+PAR_NIGHT = 5
 
 PBAND1 = 4
 PBAND2 = 10 
@@ -20,7 +26,7 @@ V = 6
 
 
 def set_ventSet(par):
-    return VENTSET1 if par < PAR else VENTSET2
+    return VENTSET1 if par < PAR_NIGHT else VENTSET2
 
 
 def set_pband(T_out, Vel):
@@ -37,9 +43,10 @@ def set_pband(T_out, Vel):
     return Pband - Pband_corr
 
 
-def control_u9(T_out, par):
+def control_u9(T_out, par, Vel):
+    '''Control del sistema de niebla'''
     VentSet = set_ventSet(par)
-    Pband = set_pband(T_out)
+    Pband = set_pband(T_out, Vel)
     if T_out < VentSet:
         return 0.0
     elif T_out > VentSet + Pband:
@@ -47,6 +54,18 @@ def control_u9(T_out, par):
     else:
         return (T_out - VentSet)/Pband
 
+
+def control_u8(vpd, vpd_set):
+    '''Control de los respiraderos en el techo'''
+    I = integral(vpd - vpd_set)
+    error = vpd[-1] - vpd_set
+    if control_u8.old_error is None:
+        control_u8.old_error = error
+    d = error - control_u8.old_error
+    control_u8.old_error = error
+    control = pid(error, KP8, KI8, KD8, I, d)
+    U_min = 0.02 if vpd < 0.5 else 0
+    return min(control + U_min, 1) 
 
 def pid(error, kp, ki, kd, I, d):
     return kp * error + ki * I + kd * d
@@ -84,11 +103,6 @@ def control_u10(x, set_):
     return control(error, KP10, KI10, KD10, I, d)
 
 
-def control_u8(x, VPD):
-    pass
-
-
-
 class agent_baseline():
     def __init__(self, env):
         self.env     = env
@@ -96,15 +110,16 @@ class agent_baseline():
         self.co2     = 0
         self.reset()
 
-    def get_action(self, state, T_out):
+    def get_action(self, state, T_out, par, Vel):
         U    = np.zeros(11)
         U[0] = self.u1(state, T_out) #U1
         T2_list = np.array([float(y) for y in self.env.daily_T2[-5:]])
         C1_list = np.array([float(y) for y in self.env.daily_C1[-5:]])
         self.set_point_t2(state)
         self.set_point_co2(state)
-        U[10] = control_u4(T2_list,self.heatset) #Ultimos 5 min        U4
-        U[9] = control_u10(C1_list,self.co2)    #Ultimos 5 min        U10
+        U[10] = control_u4(T2_list, self.heatset) #Ultimos 5 min        U4
+        U[9] = control_u10(C1_list, self.co2)    #Ultimos 5 min        U10
+        U[8] = control_u9(T_out, par, Vel)
         self.reset()
         return U
 
@@ -131,4 +146,5 @@ class agent_baseline():
 
     def reset(self):
         control_u10.old_error = None
+        control_u8.old_error = None
         
