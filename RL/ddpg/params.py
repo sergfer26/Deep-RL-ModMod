44690b3a6fbf007369f5a42ea2d12936d4625a50
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-11-07 20:12:20 -0600

    Se cambio la arquitectura de la red

--- RL/ddpg/params.py
+++ RL/ddpg/params.py
@@ -7,7 +7,7 @@ dir_path+= '/RL'
 sys.path.append(os.path.abspath(os.path.join(dir_path, os.pardir)))
 from RL.params import PARAMS_ENV
 
-PARAMS_DDPG = {'hidden_sizes': [64, 64], 'actor_learning_rate': 1e-4, 
+PARAMS_DDPG = {'hidden_sizes': [64,64,64], 'actor_learning_rate': 1e-4, 
     'critic_learning_rate': 1e-3, 'gamma':0.98, 'tau':0.125, 'max_memory_size':int(1e5)}
 
 PARAMS_UTILS = {'mu':0.0, 'theta': 0.00, 'max_sigma':0.01, 'min_sigma': 0.0, 'decay_period':int(PARAMS_ENV['TIME_MAX']*PARAMS_ENV['STEP']**-1)}
