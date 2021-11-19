afce84874147b0759111c7a51d21286a63f3734a
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-11-19 14:23:21 -0600

    Se agregaron parametros de simulacion y funcion para guardarlos

--- RL/params.py
+++ RL/params.py
@@ -1,6 +1,7 @@
+import json
 minutos = 60
 PARAMS_ENV = {'STEP': minutos/(24*60), \
-              'TIME_MAX': 90, #Dias simulados\ 
+              'TIME_MAX': 1, #Dias simulados\ 
               'FRECUENCY': 60, 
               'SEASON':1}
 
@@ -8,23 +9,46 @@ PARAMS_ENV = {'STEP': minutos/(24*60), \
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
-PARAMS_TRAIN = {'EPISODES': 1000, \
+PARAMS_TRAIN = {'EPISODES': 1, \
                 'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
+                'SERVER':False, \
                 'INDICE': 0, # Cuando es distinto de 0, fija un indice para simular
                 'SAVE_FREQ': 1000
                 } 
 
+PARAMS_SIM = {'anio':2017,\
+            'mes':1,\
+            'dia': 1,\
+            'hora': 1
+            }
+            
 CONTROLS = {'u_1': 1, 
             'u_2': 0, 
             'u_3': 0, 
-            'u_4': 1,   
+            'u_4': 0,   
             'u_5': 0, 
             'u_6': 0, 
             'u_7': 0, 
             'u_8': 1,  
             'u_9': 1, 
             'u_10': 1,
-            'u_11': 0, 
+            'u_11': 1, 
             }
+f = open ('ddpg/parametros.json', "r")
+parametros_ddpg = json.loads(f.read())
+
+all_params = {
+            'PARAMS_DDPG':parametros_ddpg['PARAMS_DDPG'], \
+            'PARAMS_UTILS':parametros_ddpg['PARAMS_UTILS'], \
+            'PARAMS_ENV': PARAMS_ENV, \
+            'PARAMS_TRAIN':PARAMS_TRAIN,\
+            'PARAMS_SIM':PARAMS_SIM, \
+            'CONTROLS': CONTROLS}
+            
+def save_params(dicc,path):
+    with open(path + '/parametros.json', 'w') as outfile:
+        json.dump(dicc, outfile,indent=4)
+
+
