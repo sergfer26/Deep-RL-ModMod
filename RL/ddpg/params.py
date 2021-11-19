99c039aa8a686d12d0b246e2304e6c9d4437576e
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-11-19 14:28:42 -0600

    Se guardan los parametros en un json

--- RL/ddpg/params.py
+++ RL/ddpg/params.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 dir_path = os.path.dirname(os.path.realpath('../RL'))
 dir_path+= '/RL'
 
@@ -9,7 +10,10 @@ from RL.params import PARAMS_ENV
 
 
 PARAMS_DDPG = {'hidden_sizes': [64,64,64], 'actor_learning_rate': 1e-4, 'critic_learning_rate': 1e-3, 'gamma':0.98, 'tau':0.125, 'max_memory_size':int(1e5)}
-
 PARAMS_UTILS = {'mu':0.0, 'theta': 0.00, 'max_sigma':0.01, 'min_sigma': 0.0, 'decay_period':int(PARAMS_ENV['TIME_MAX']*PARAMS_ENV['STEP']**-1)}
 
+params = {'PARAMS_DDPG':PARAMS_DDPG,'PARAMS_UTILS':PARAMS_UTILS}
+
 #El parametro decay_period debe ser menor o igual al numero de pasos por episodio
+with open("ddpg/parametros.json", "w") as outfile:
+    json.dump(params, outfile)
