4568e4928d7b99d7251e1497bde2ff56244b0cbc
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-11-25 22:10:54 -0600

    se agregaron mas episodios

--- RL/params.py
+++ RL/params.py
@@ -2,16 +2,16 @@ import json
 from climate_model.constants import OTHER_CONSTANTS
 minutos = 60
 PARAMS_ENV = {'STEP': minutos/(24*60), \
-              'TIME_MAX': 2, #Dias simulados\ 
+              'TIME_MAX': 90, #Dias simulados\ 
               'FRECUENCY': 60, 
-              'SEASON':2, \
+              'SEASON':1, \
               'MINUTOS':minutos}
 
 #Para entrenamiento SEASON puede ser 1,2 o 'RANDOM'
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
-PARAMS_TRAIN = {'EPISODES': 2, \
+PARAMS_TRAIN = {'EPISODES': 1000, \
                 'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
