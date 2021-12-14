b70baa36f2f559544647a5c5d8a9ba8a8146526a
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2021-12-13 22:41:05 -0600

    Se regreso a parametros de entrenamiento

--- RL/params.py
+++ RL/params.py
@@ -2,7 +2,7 @@ import json
 from climate_model.constants import OTHER_CONSTANTS
 minutos = 60
 PARAMS_ENV = {'STEP': minutos/(24*60), \
-              'TIME_MAX': 1, #Dias simulados\ 
+              'TIME_MAX': 90, #Dias simulados\ 
               'FRECUENCY': 60, 
               'SEASON':1, \
               'MINUTOS':minutos}
@@ -11,13 +11,13 @@ PARAMS_ENV = {'STEP': minutos/(24*60), \
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
-PARAMS_TRAIN = {'EPISODES': 2, \
+PARAMS_TRAIN = {'EPISODES': 1000, \
                 'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
                 'SERVER':True, \
                 'INDICE': 0, # Se usa en la simulacion al terminar el entrenamiento
-                'SAVE_FREQ': 1000
+                'SAVE_FREQ': 200
                 } 
 
 PARAMS_SIM = {'anio':2017,\
